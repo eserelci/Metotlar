62282ae4634da141b92bc098b84ee1858ba98bb0
Eser <47974991+eserelci@users.noreply.github.com>
2021-03-07 22:27:11 +0300

    Ödev yapıldı

---
+++ OOP1/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //Snippet hazır kodlar demektir.
+    public class Product
+    {
+        public int Id { get; set; }//Primary Key Referansıdır.
+        public int CategoryId { get; set; } //Foreign Key Referans Alanı.
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        //CRUD Create Update Delete İşlemleri olabilir.
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;//2 Mobilyaya karşılık gelsin örnek olarak.
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+            Product product2 = new Product
+            {
+                Id=2,
+                CategoryId=5,
+                ProductName="Kalem",
+                UnitsInStock=5,
+                UnitPrice=35
+            };
+            //PascalCase   CamelCase
+            //Case Sensitive Küçük büyük harfe duyarlıdır C#.
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+            //int,double,bool,decimal,float... değer tiptir.
+            //Arrayler,class,abstract class,interface referans tiptir.
+        }
+    }
+}
